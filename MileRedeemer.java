bcfa1903ec79f7b4b3fd94cef5dfeaeb1b23938c
Alla Sandeep <sandeepalla96@gmail.com>
2019-04-16 19:53:24 -0500

    Update MileRedeemer.java

--- MileRedeemer.java
+++ MileRedeemer.java
@@ -1,17 +1,3 @@
-/*******************************************************************************************************
-  *                                                                                                  *
-  *  CSCI 470/502          Assignment 8           Fall 2018                                          *
-  *                                                                                                  *
-  * Programmer : Sandeep Alla                                                                        *
-  *                                                                                                  *
-  * Due Date : 11/28/2018 11:59PM                                                                    *
-  *                                                                                                  *                                                           *
-  * Purpose : This program contains a "MileRedemption" class which has various methods to calculate  *
-  * and return miles and possible tickets to various destinations                                    *
-  *                                                                                                  *
-  *******************************************************************************************************/
-
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
